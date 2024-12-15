b2beae902378a00e49ef8248850a9e0a0d0d7d2a
Andrew Serra <acs8929@rit.edu>
2024-12-14 23:45:01 -0500

    created server and daemon application directories

---
+++ cmd/testsource-server/main.go
@@ -0,0 +1,10 @@
+/*
+ * Created on Sat Dec 14 2024
+ *
+ * Copyright © 2024 Andrew Serra <andy@serra.us>
+ */
+package main
+
+func main() {
+
+}
---
+++ cmd/testsourced/main.go
@@ -0,0 +1,11 @@
+/*
+ * Created on Sat Dec 14 2024
+ *
+ * Copyright © 2024 Andrew Serra <andy@serra.us>
+ */
+
+package main
+
+func main() {
+
+}
