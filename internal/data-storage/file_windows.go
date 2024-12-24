30602a40e1c01b3f549e0ac79466686ac2381980
Andrew Serra <acs8929@rit.edu>
2024-12-24 17:14:53 +0300

    created windows version of file operations

--- internal/data-storage/file_windows.go
+++ internal/data-storage/file_windows.go
@@ -17,28 +17,53 @@ import (
 )
 
 const PATH_SEPARATOR string = "\\"
-const CONFIG_FILE_NAME string = ".cstc"
+const PROFILE_FILE_NAME string = ".cstc_profiles"
+const STATE_FILE_NAME string = "state"
+
+type systemState struct {
+	CurrentProfile string            `json:"active_profile"`
+	Data           map[string]string `json:"data"`
+}
 
 type UserProfile struct {
 	Id    string `json:"id,omitempty"`
 	Email string `json:"email,omitempty"`
 }
 
-func init() {
-	// configFile := fmt.Sprintf("%s%s%s", "$HOME", PATH_SEPARATOR, CONFIG_FILE_NAME)
-	configFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, CONFIG_FILE_NAME)
+var state *systemState
 
-	if _, err := os.Stat(configFile); errors.Is(err, fs.ErrNotExist) {
-		fmt.Println("config file does not exist")
-		os.Create(configFile)
+func init() {
+	profileFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, PROFILE_FILE_NAME)
+	stateFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, STATE_FILE_NAME)
+
+	for _, file := range []string{stateFile, profileFile} {
+		if _, err := os.Stat(file); errors.Is(err, fs.ErrNotExist) {
+			f, err := os.Create(file)
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			var emptyDoc map[string]UserProfile = make(map[string]UserProfile)
+			err = json.NewEncoder(f).Encode(&emptyDoc)
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
 	}
+
+	state = new(systemState)
+	loadState()
 }
 
-func getConfig() (map[string]UserProfile, error) {
-	// configFile := fmt.Sprintf("%s%s%s", "$HOME", PATH_SEPARATOR, CONFIG_FILE_NAME)
-	configFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, CONFIG_FILE_NAME)
+// File ops
+func getStoredProfiles() (map[string]UserProfile, error) {
+	profileFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, PROFILE_FILE_NAME)
 
-	data, err := os.ReadFile(configFile)
+	data, err := os.ReadFile(profileFile)
 	if err != nil {
 		return nil, err
 	}
@@ -52,23 +77,51 @@ func getConfig() (map[string]UserProfile, error) {
 	return userProfiles, nil
 }
 
-func saveConfig(userProfiles map[string]UserProfile) error {
-	// configFile := fmt.Sprintf("%s%s%s", "$HOME", PATH_SEPARATOR, CONFIG_FILE_NAME)
-	configFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, CONFIG_FILE_NAME)
+func saveProfiles(userProfiles map[string]UserProfile) error {
+	stateFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, PROFILE_FILE_NAME)
 
 	data, err := json.Marshal(userProfiles)
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(configFile, data, 0644)
+	return os.WriteFile(stateFile, data, 0644)
 }
 
-func GetUserProfile(profileName string) (*UserProfile, error) {
-	userProfiles, err := getConfig()
+func loadState() error {
+	data, err := os.ReadFile(fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, STATE_FILE_NAME))
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, state)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func saveState() error {
+	stateFile := fmt.Sprintf("%s%s%s", ".", PATH_SEPARATOR, STATE_FILE_NAME)
+
+	data, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(stateFile, data, 0644)
+}
+
+// Profile File Operations
+func GetActiveUserProfile() (*UserProfile, error) {
+	userProfiles, err := getStoredProfiles()
 	if err != nil {
 		return nil, err
 	}
+	profileName := GetActiveProfileName()
 
 	for key, userProfile := range userProfiles {
 		if key == profileName {
@@ -79,21 +132,89 @@ func GetUserProfile(profileName string) (*UserProfile, error) {
 }
 
 func CreateNewUserProfile(profileName string, profile UserProfile) error {
-	userProfiles, err := getConfig()
+	userProfiles, err := getStoredProfiles()
 	if err != nil {
 		return err
 	}
 
 	userProfiles[profileName] = profile
-	return saveConfig(userProfiles)
+	if state.CurrentProfile == "" {
+		state.CurrentProfile = profileName
+		if err = saveState(); err != nil {
+			return err
+		}
+	}
+	return saveProfiles(userProfiles)
 }
 
 func DeleteUserProfile(profileName string) error {
-	userProfiles, err := getConfig()
+	userProfiles, err := getStoredProfiles()
 	if err != nil {
 		return err
 	}
 
 	delete(userProfiles, profileName)
-	return saveConfig(userProfiles)
+
+	if len(userProfiles) == 0 {
+		state.CurrentProfile = ""
+		if err = clearActiveProfileState(); err != nil {
+			return err
+		}
+	}
+	return saveProfiles(userProfiles)
+}
+
+func GetUserProfileList() ([]string, error) {
+	userProfiles, err := getStoredProfiles()
+	if err != nil {
+		return nil, err
+	}
+
+	keys := make([]string, 0, len(userProfiles))
+	for key := range userProfiles {
+		keys = append(keys, key)
+	}
+
+	return keys, nil
+}
+
+func IsEmailUsedInProfile(email string) (bool, error) {
+	userProfiles, err := getStoredProfiles()
+	if err != nil {
+		return false, err
+	}
+
+	for _, userProfile := range userProfiles {
+		if userProfile.Email == email {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// State File Operations
+func SetNewActiveProfileState(profile string) error {
+	userProfiles, err := getStoredProfiles()
+	if err != nil {
+		return err
+	}
+
+	for _, p := range userProfiles {
+		if p.Id == profile {
+			state.CurrentProfile = p.Id
+			saveState()
+			return nil
+		}
+	}
+	return fmt.Errorf("user profile '%s' not found", profile)
+}
+
+func clearActiveProfileState() error {
+	state.CurrentProfile = ""
+	saveState()
+	return nil
+}
+
+func GetActiveProfileName() string {
+	return state.CurrentProfile
 }
